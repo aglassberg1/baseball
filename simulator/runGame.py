d2d5140c25b8319b197a0926f5119e3271a2172f
aglassberg1 <125155178+aglassberg1@users.noreply.github.com>
2023-02-14 00:09:18 -0500

    Update runGame.py

--- simulator/runGame.py
+++ simulator/runGame.py
@@ -1,23 +1,23 @@
 import inning
 import team
 
-inning_count = 0
-inning_half = 1
-away_team = team.Yankees()
-home_team = team.Mets()
-batter = [0, 0]
-runs = [0, 0]
-hits = [0, 0]
-errors = [0, 0]
+inning_count = 0                    #keeps track of the inning
+inning_half = 1                     #keeps track fo the inning half
+away_team = team.Yankees()          #sets the away team
+home_team = team.Mets()             #sets the home team
+batter = [0, 0]                     #tracks the current position of the lineup [away, home]
+runs = [0, 0]                       #tracks the score
+hits = [0, 0]                       #tracks the number of hits
+errors = [0, 0]                     #tracks the number of errors
 
 
-away_team.print_lineup()
+away_team.print_lineup()            #prints the lineup for the away team
+print()     
+home_team.print_lineup()            #prints the lineup for the home team
 print()
-home_team.print_lineup()
-print()
 
 
-def game_over():
+def game_over():                    #checks end of game conditions
     if inning_count >= 9 and inning_half == 1 and runs[0] != runs[1]:
         return True
     elif inning_count >= 9 and inning_half == 0 and runs[1] > runs[0]:
@@ -26,8 +26,8 @@ def game_over():
         return False
 
 
-while game_over() == False:
-    match inning_half:
+while game_over() == False:         #simulation code
+    match inning_half:              #creates the current half inning
         case 0:
             inning_half = 1
             current_inning = inning.Inning(inning_half, inning_count, home_team, away_team, runs[1], runs[0], batter[1])
