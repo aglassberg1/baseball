216a52abeae49cb0262c648e26f227a335f16cdc
aglassberg1 <125155178+aglassberg1@users.noreply.github.com>
2023-02-13 23:59:55 -0500

    Update player.py

--- simulator/player.py
+++ simulator/player.py
@@ -3,11 +3,11 @@ import baseballDict as bbdic
 
 class Player:
 
-    def __init__(self, posnum, firstname, lastname):
+    def __init__(self, posnum, firstname, lastname):            #defines Player object
         self.firstname = firstname
         self.lastname = lastname
-        self.position = bbdic.dic_position[posnum]
-        self.posAbbrev = bbdic.dic_posAbbrev[self.position]
+        self.position = bbdic.dic_position[posnum]              #sets player position
+        self.posAbbrev = bbdic.dic_posAbbrev[self.position]     #sets position abbreviation
 
-    def name(self):
-        return self.firstname + " " + self.lastname
+    def name(self):                                             #returns name of the player
+        return self.firstname + " " + self.lastname             
